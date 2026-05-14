a4065420763f21b874a5815026b06e39c90b0675
techift <esugift62@gmail.com>
2026-05-14 12:11:44 +0100

    chore: added some widgets and styling

--- lib/features/counter/view/home_page.dart
+++ lib/features/counter/view/home_page.dart
@@ -1,37 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:naija_wallet/features/counter/providers/counter_provider.dart';
+import 'package:naija_wallet/features/counter/view/details_page.dart';
 
-class MyWidget extends ConsumerWidget {
-  const MyWidget({super.key});
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final counter = ref.watch(counterProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Center(
+          child: Text(
+            'Home Page',
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
         backgroundColor: Colors.green,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const DetailsPage()),
+              );
+            },
+            icon: const Icon(Icons.arrow_forward_ios, color: Colors.white),
+          ),
+        ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
             '$counter',
-            style: const TextStyle(fontSize: 24),
+            style: const TextStyle(fontSize: 55, fontWeight: FontWeight.bold),
           ),
-          ElevatedButton(
-            onPressed: () {
-              ref.read(counterProvider.notifier).increment();
-            },
-            child: const Text('+'),
-          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  ref.read(counterProvider.notifier).increment();
+                },
+                child: const Text(
+                  '+',
+                  style: TextStyle(fontSize: 25, color: Colors.white),
+                ),
+              ),
 
-          ElevatedButton(
-            onPressed: () {
-              ref.read(counterProvider.notifier).decrement();
-            },
-            child: const Text('-'),
+              const SizedBox(
+                width: 30,
+              ),
+
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  ref.read(counterProvider.notifier).decrement();
+                },
+                child: const Text(
+                  '-',
+                  style: TextStyle(fontSize: 25, color: Colors.white),
+                ),
+              ),
+            ],
           ),
         ],
       ),
