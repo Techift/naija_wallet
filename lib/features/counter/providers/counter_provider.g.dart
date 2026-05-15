599a62dc41eb7677846b40b8a3f60a1f0a5a6653
techift <esugift62@gmail.com>
2026-05-15 22:20:38 +0100

    chore: run build_runner to update generated files

--- lib/features/counter/providers/counter_provider.g.dart
+++ lib/features/counter/providers/counter_provider.g.dart
@@ -6,11 +6,11 @@ part of 'counter_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$counterHash() => r'4320f811608c7a6e7342b83e3031965a34f7cb8e';
+String _$counterHash() => r'90023ba5535dfbfd4e56f7ad26bc1d7669fcb510';
 
 /// See also [Counter].
 @ProviderFor(Counter)
-final counterProvider = AutoDisposeNotifierProvider<Counter, int>.internal(
+final counterProvider = AutoDisposeAsyncNotifierProvider<Counter, int>.internal(
   Counter.new,
   name: r'counterProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -20,6 +20,6 @@ final counterProvider = AutoDisposeNotifierProvider<Counter, int>.internal(
   allTransitiveDependencies: null,
 );
 
-typedef _$Counter = AutoDisposeNotifier<int>;
+typedef _$Counter = AutoDisposeAsyncNotifier<int>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
