d64fb97abd93ac9843342b9fbd92efe521a0f587
techift <esugift62@gmail.com>
2026-05-24 19:19:20 +0100

    chore: add home screen file

---
+++ lib/features/home/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Text(
+          'Home Screen',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        ),
+      )
+    );
+  }
+}
