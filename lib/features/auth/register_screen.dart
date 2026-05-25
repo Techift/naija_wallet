30df7f6a60fe1d705bfdf5311c9546bf29c025aa
techift <esugift62@gmail.com>
2026-05-25 15:52:25 +0100

    chore: add register screen

---
+++ lib/features/auth/register_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:naija_wallet/features/auth/auth_service.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: (){
+              AuthService.register();
+              context.go('/');
+            },child: const Text('Register'))
+          ],
+        ),
+      )
+    );
+  }
+}
