49e534664150fdb242e037483dab2e5d95f5e0c0
techift <esugift62@gmail.com>
2026-05-25 22:54:06 +0100

    refactor: extract hardcoded colors into AppColors

--- lib/features/splash/view/splash_screen.dart
+++ lib/features/splash/view/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:naija_wallet/core/theme/app_color.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -22,6 +23,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: AppColors.lightBackground,
       body: Center(
         child: Text('Naija Wallet'),
       )
