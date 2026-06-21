07fdfb3011a3d842cfaabaa7820c171d40984ce2
techift <esugift62@gmail.com>
2026-06-21 20:25:26 +0100

    chore: few code changes

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -9,6 +9,7 @@ import 'package:naija_wallet/features/splash/view/splash_screen.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: Routes.splash,
+  // initialLocation: Routes.login,
   
   redirect:(context, state) {
     final loggedIn = AuthService.isLoggedIn;
